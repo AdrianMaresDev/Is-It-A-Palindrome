de26781ff3172b4d2bb42213b60ac4f338f771f8
AdrianMares <adrianmares.dev@gmail.com>
2024-08-14 18:00:13 -0400

    finished palindrome checker

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const input = document.getElementById("input")
+const answer = document.getElementById("check-text")
 
 
 //This function makes sure that the user input is always converted to lower case before it is checked.
@@ -8,7 +9,22 @@ function isPalindrome(str) {
     return clean === clean.split('').reverse().join('');
 }
 
+//This function takes in the user input and runs the isPalindrome function.
 function checkPalindrome() {
-    const value = input.value;
+    const value = input.value.trim();
+
+    if (value === "") {
+        answer.innerText = "Please enter a word."
+        return
+    }
+
     const reversed = isPalindrome(value);
+
+    if (reversed) {
+        answer.innerText = "This is a Palindrome!";
+    } else {
+        answer.innerText = "This is not a Palindrome. Try again?"
+    }
 }
+
+document.querySelector("#check-button").addEventListener("click", checkPalindrome);
